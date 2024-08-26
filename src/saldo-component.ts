f31ca45354531e15b3dc5da99cf245ee20dfb20d
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-08-26 15:46:14 -0300

    criação de tipoTransação
    criação de tipoTransação

--- src/saldo-component.ts
+++ src/saldo-component.ts
@@ -1,7 +1,14 @@
-let saldo = 3000;
+let saldo: number = 3000;
 
 const elementoSaldo = document.querySelector(".saldo-valor .valor") as HTMLElement;
+const elementoDataAcesso = document.querySelector(".block-saldo time") as HTMLElement;
+
+
 if (elementoSaldo != null) {
-    elementoSaldo.textContent = saldo.toString();
+  elementoSaldo.textContent = saldo.toLocaleString ("pt-br", { currency: "BRL", style: "currency" });
 }
-  
+
+if (elementoDataAcesso != null){
+    const elementoDataAcesso: Date = new Date();
+    elementoDataAcesso.textContent = formatarData (dataAcesso)
+}
