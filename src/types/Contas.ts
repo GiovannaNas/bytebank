95b274bc07eabc9bc9c42176416cbf4586d17f1a
Giovanna Alves Do Nascimento <134456621+GiovannaNas@users.noreply.github.com>
2024-08-27 15:41:17 -0300

    update GrupoTransacao
    update GrupoTransacao

--- src/types/Contas.ts
+++ src/types/Contas.ts
@@ -2,65 +2,74 @@ import { Transacao } from "./Transacao.js";
 import { TipoTransacao } from "./TipoTransacao.js";
 import { GrupoTransacao } from "./GrupoTransacao.js";
 
-let saldo: number = JSON.parse (localStorage.getItem("saldo")) || 0 ;
-const transacoes:Transacao[] = JSON.parse(localStorage.getItem("transacoes"),(key:string, value:string) => {
-    if (key === "data") {
+let saldo: number = JSON.parse(localStorage.getItem("saldo")) || 0;
+const transacoes: Transacao[] =
+  JSON.parse(
+    localStorage.getItem("transacoes"),
+    (key: string, value: string) => {
+      if (key === "data") {
         return new Date(value);
+      }
+      return value;
     }
-    return value; 
-} ) || [];
+  ) || [];
 
-function debitar(valor:number): void {
-    if(valor <= 0 ){
-        throw new Error ("O valor debitado deve ser maior que zero!");
-    }
-if (valor > saldo){
-    throw new Error ("Saldo insuficiente");
+function debitar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error("O valor debitado deve ser maior que zero!");
+  }
+  if (valor > saldo) {
+    throw new Error("Saldo insuficiente");
+  }
+
+  saldo -= valor;
+  localStorage.setItem("saldo", saldo.toString());
 }
 
-saldo -= valor;
-localStorage.setItem("saldo", saldo.toString());
-}
+function depositar(valor: number): void {
+  if (valor <= 0) {
+    throw new Error("O valor debitado deve ser maior que zero!");
+  }
 
-function depositar(valor:number): void {
-    if (valor <= 0 ){
-        throw new Error ("O valor debitado deve ser maior que zero!");
-    }
-
-saldo += valor; 
-localStorage.setItem("saldo", saldo.toString());
+  saldo += valor;
+  localStorage.setItem("saldo", saldo.toString());
 }
 
 const Conta = {
-    getSaldo(){
-        return saldo;
-    },
+  getSaldo() {
+    return saldo;
+  },
 
-    getDataAcesso(): Date {
-        return new Date();
-    }, 
+  getDataAcesso(): Date {
+    return new Date();
+  },
 
-    getGrupoTransacoes(): GrupoTransacao[] {
-const GrupoTransacoes: GrupoTransacao[] = [];
-const listaTransacoes: Transacao[] = structuredClone (transacoes);
-const transacoesOrdenadas: Transacao[] = listaTransacoes.sort ((t1, t2) => t2.data.getTime () - t1.data.getTime() )
-    },
+  getGrupoTransacoes(): GrupoTransacao[] {
+    const GrupoTransacoes: GrupoTransacao[] = [];
+    const listaTransacoes: Transacao[] = structuredClone(transacoes);
+    const transacoesOrdenadas: Transacao[] = listaTransacoes.sort((t1, t2) => t2.data.getTime() - t1.data.getTime());
+    let labelAtualGrupoTransacao: string = "";
 
-    registrarTransacao(novaTransacao: Transacao): void {
-        if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
-       depositar(novaTransacao.valor);
-    } 
-    else if (novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA || novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO) {
-       debitar(novaTransacao.valor);
-    } 
-    else {
-       throw new Error ("Tipo de Transação é inválido!");
+    for (let transacao of transacoesOrdenadas) {
+        let labelAtualGrupoTransacao: string = transacao.data.toLocaleDateString ("pt-br", {month:"long", year: })
+    }
+  },
+
+  registrarTransacao(novaTransacao: Transacao): void {
+    if (novaTransacao.tipoTransacao == TipoTransacao.DEPOSITO) {
+      depositar(novaTransacao.valor);
+    } else if (
+      novaTransacao.tipoTransacao == TipoTransacao.TRANSFERENCIA ||
+      novaTransacao.tipoTransacao == TipoTransacao.PAGAMENTO_BOLETO
+    ) {
+      debitar(novaTransacao.valor);
+    } else {
+      throw new Error("Tipo de Transação é inválido!");
     }
     transacoes.push(novaTransacao);
     console.log(transacoes);
-    localStorage.setItem ("transacoes", JSON.stringify(transacoes))
-    }
-}
+    localStorage.setItem("transacoes", JSON.stringify(transacoes));
+  },
+};
 
 export default Conta;
-
